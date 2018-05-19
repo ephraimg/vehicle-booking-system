fb799611ca87f0d10cb3944ec8e1507b623229f1
ephraimg <ephraim.glick@gmail.com>
2018-05-19 23:02:04 +0100

    Separate components from containers, start on details

--- client/src/app/components/JobCreate.jsx
+++ client/src/app/components/JobCreate.jsx
@@ -1,7 +1,46 @@
 
 import React from 'react';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
 
-export const JobCreate = props => (
+const JobCreate = props => (
     <div>  
         <h1>Create a new job</h1>
-    </div> );
+        <form>
+            <div>
+                <label for="date">Select a date:</label>
+                <DatePicker name="date"
+                    selected={props.date}
+                    onChange={props.handleDateChange}
+                />
+            </div>
+            <div>
+                <label for="start">Select a start time:</label>
+                <select name="start" selected={props.time} onChange={props.handleTimeChange}>
+                    { Array(24).fill(0).map((el, idx) =>
+                        <option value={idx}>{('0' + idx).slice(-2) + ':00'}</option>)
+                    }
+                </select>
+            </div>
+            <div>
+                <label for="duration">Select a duration:</label>
+                <select name="duration" selected={props.duration} onChange={props.handleDurChange}>
+                    { Array(12).fill(0).map((el, idx) =>
+                        <option value={idx + 1}>{`${idx + 1} hours`}</option>)
+                    }
+                </select>
+            </div>
+            <button type="button" onClick={props.createJob}>
+                Request booking
+            </button>
+        </form>
+        <div>
+            { props.jobWaiting ? 'Please wait a moment...' : null }
+            { props.jobSuccess ? 'Job saved successfully!' : null }
+            { props.jobFailure ? 'Error! Please try again.' : null }
+        </div>
+    </div>
+);
+
+
+export { JobCreate };
--- client/src/app/components/VehicleCreate.jsx
+++ client/src/app/components/VehicleCreate.jsx
@@ -1,7 +1,45 @@
 
 import React from 'react';
+import axios from 'axios';
 
-export const VehicleCreate = props => (
+const VehicleCreate = props => (
     <div>  
         <h1>Create a new truck</h1>
-    </div> );
+        <form>
+            <div>
+                <label for="name">Provide a name:</label>
+                <input name="name" value={props.name} 
+                    placeholder="Anonymous vehicle"
+                    onChange={props.handleNameChange}>
+                </input>
+            </div>
+            <div>
+                <label for="start">Select its daily starting time:</label>
+                <select name="start" value={props.start} onChange={props.handleStartChange}>
+                    { Array(24).fill(0).map((el, idx) =>
+                        <option value={idx}>{('0' + idx).slice(-2) + ':00'}</option>)
+                    }
+                </select>
+            </div>
+            <div>
+                <label for="stop">Select its daily stopping time:</label>
+                <select name="stop" value={props.stop} onChange={props.handleStopChange}>
+                    { Array(24).fill(0).map((el, idx) => 
+                        <option value={idx}>{('0' + idx).slice(-2) + ':00'}</option>)
+                    }
+                </select>
+            </div>
+            <button type="button" onClick={props.createVehicle}>
+                Save vehicle
+            </button>
+        </form>
+        <div>
+            { props.vehicleWaiting ? 'Please wait a moment...' : null }
+            { props.vehicleSuccess ? 'Vehicle saved successfully!' : null }
+            { props.vehicleFailure ? 'Error! Please try again.' : null }
+        </div>
+    </div>
+);
+
+export { VehicleCreate };
+
