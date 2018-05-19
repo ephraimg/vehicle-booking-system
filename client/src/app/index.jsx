48c2bb76386085b2e6cdffaea1d2a277a9ba8779
ephraimg <ephraim.glick@gmail.com>
2018-05-19 22:58:22 +0100

    Merge App into index

--- client/src/app/index.jsx
+++ client/src/app/index.jsx
@@ -3,11 +3,19 @@ import '../styles.css'; // to let webpack plugin find CSS
 
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { App } from './components/App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Home } from './components/Home';
+import { VehicleCreate } from './containers/VehicleCreate';
+import { VehicleList } from './containers/VehicleList';
+import { JobCreate } from './containers/JobCreate';
 
 render((
     <BrowserRouter>
-        <App />
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route path="/VehicleCreate" component={VehicleCreate}/>
+            <Route path="/VehicleList" component={VehicleList}/>
+            <Route path="/JobCreate" component={JobCreate}/>
+        </Switch>
     </BrowserRouter>
 ), document.getElementById('app'));
